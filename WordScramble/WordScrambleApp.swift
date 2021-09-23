69b49da64f68b7c709bc6610fe27a499a5d04724
Greg Wu <mrgrwu@gmail.com>
2021-09-23 10:17:40 -0700

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Greg Wu on 9/23/21.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
